cbb6035f468d2cdbf7ea06bc6703097d20a5baaf
younes_damouna <younes.damouna@gmail.com>
2023-12-15 23:41:09 +0200

    added InputGroup component

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import InputGroup from "../../components/InputGroup";
+
+const HomePage = () => {
+  return (
+    <div>
+      <InputGroup
+        label="Email"
+        onChange={() => {}}
+        name="email"
+        
+        type="email"
+        className=""
+        placeholder="Email"
+        id=""
+      />
+    </div>
+  );
+};
+
+export default HomePage;
