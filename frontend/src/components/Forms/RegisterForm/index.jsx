0d62fef933ab1df6acba586048c81d5b5141992a
younes_damouna <younes.damouna@gmail.com>
2023-12-21 15:55:10 +0200

    linked registration with backend

--- frontend/src/components/Forms/RegisterForm/index.jsx
+++ frontend/src/components/Forms/RegisterForm/index.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./index.css";
 import InputGroup from "../../InputGroup";
 import Button from "../../Button";
+import { sendRequest } from "../../../core/request";
 const RegisterForm = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    name: "",
+    lastName: "",
+    userType:1
+  });
+
+  const handleForm = (field, value) => {
+    setForm((prev) => {
+      return {
+        ...prev,
+        [field]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async () => {
+    const body = { ...form, last_name: form.lastName };
+    const response = await sendRequest({
+      body: body,
+      route: "auth/register",
+      method: "POST",
+    });
+
+    console.log(response?.status);
+  };
   return (
     <div className="w-100 ">
       <div className="text-white d-flex center fs-lg">Register</div>
@@ -19,9 +47,8 @@ const RegisterForm = () => {
           label={"First Name"}
           name="first_name"
           placeholder="John"
-          onChange={() => {
-            // onChange
-          }}
+          onChange={(e) => handleForm("firstName", e.target.value)}
+
         />
          <InputGroup
           id={"last_name"}
@@ -29,9 +56,8 @@ const RegisterForm = () => {
           label={"Last Name"}
           name="last_name"
           placeholder="Doe"
-          onChange={() => {
-            // onChange
-          }}
+          onChange={(e) => handleForm("lastName", e.target.value)}
+
         />
 
         </div>
@@ -41,9 +67,8 @@ const RegisterForm = () => {
           placeholder={"john.doe@example.com"}
           name="email"
           label="Email"
-          onChange={() => {
-            // onChange
-          }}
+          onChange={(e) => handleForm("email", e.target.value)}
+
         />
         <InputGroup
           id={"password"}
@@ -51,13 +76,12 @@ const RegisterForm = () => {
           placeholder=""
           name="password"
           label="Password"
-          onChange={() => {
-            // onChange
-          }}
+          onChange={(e) => handleForm("password", e.target.value)}
+
         />
 
         <div className="input-group">
-          <Button text={"Log In"} className="w-100" animation={false} />
+          <Button text={"Register"} className="w-100" animation={false} onClick={() => handleSubmit()} />
           <div className=" d-flex  justify-content-end w-100 text-white mt-1">
             Don't have an account? &nbsp;{" "}
             <span className="text-primary cursor"> Login!</span>
